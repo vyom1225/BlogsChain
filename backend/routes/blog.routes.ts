2999e371465be462af97264f7360905ded2002a2
vyom_1225 <rastogi1225@gmail.com>
2024-09-01 00:17:05 +0530

    Blog routes created

--- backend/routes/blog.routes.ts
+++ backend/routes/blog.routes.ts
@@ -18,15 +18,115 @@ export const blogRouter = new Hono<{
 //Middleware for user authentication
 blogRouter.use(authentication);
 
-blogRouter.post("/" , (c) => {
-    return c.text("Successfully uploaded the blogs")
+blogRouter.post("/" , async (c) => {
+
+    const prisma = new PrismaClient({
+        datasourceUrl : c.env.DATABASE_URL
+    }).$extends(withAccelerate())
+
+    const body = await c.req.json();
+
+    const blog = await prisma.blog.create({
+        data : {
+            title : body.title,
+            content : body.content,
+            authorID : c.get("userID")
+        }
+    })
+
+    if(!blog){
+        c.status(403)
+        return c.text("There was an error connecting to the database")
+    }
+
+    return c.json({
+        blogID : blog.id
+    })
+
 })
-blogRouter.put("/" , (c) => {
-    return c.text("Successfully updated blogs data")
+
+blogRouter.put("/" ,async (c) => {
+
+    const body = await c.req.json();
+    const ispublished = body.published === "True"
+
+    const prisma = new PrismaClient({
+        datasourceUrl : c.env.DATABASE_URL
+    }).$extends(withAccelerate());
+
+    let blog;
+
+    try{
+         blog = await prisma.blog.update({
+            where : {
+                id : body.blogID,
+                authorID : c.get("userID")
+            },
+            data : {
+                title : body.title,
+                content : body.content,
+                published : ispublished
+            }
+        })
+    }catch(e){
+        c.status(403);
+        return c.json({ msg : "There was no such BLOG"});
+    }
+   
+    return c.json(blog)
+
 })
-blogRouter.get("/:id" , (c) => {
-    return c.text("Sucessfully fetched blogs")
+
+blogRouter.get("/:id" , async (c) => {
+
+    const blogID = c.req.param("id");
+
+    const prisma = new PrismaClient({
+        datasourceUrl : c.env.DATABASE_URL
+    }).$extends(withAccelerate())
+
+    let blog;
+
+    try{
+        blog = await prisma.blog.findUnique({
+            where : {
+                id : blogID
+            }
+        })
+    }catch(e){
+        c.status(403)
+        return c.json({
+            msg : "NO such Blog was found"
+        })
+    }
+    
+    if(blog.published || c.get("userID") == blog.authorID){
+        return c.json({
+            blog
+        })
+    }
+
+    c.status(403)
+    return c.json({
+        msg : "The Blog is not availble right now"
+    })
+
 })
-blogRouter.get("/bulk" , (c) => {
-    return c.text("Sucessfully fetched blogs")
+
+blogRouter.get("/bulk" , async (c) => {
+
+    const prisma = new PrismaClient({
+        datasourceUrl : c.env.DATABASE_URL
+    }).$extends(withAccelerate());
+
+    const blogs = await prisma.blog.findMany({
+        where : {
+            authorID : c.get("userID")
+        }
+    })
+
+    return c.json({
+        blogs
+    })
+    
 })
